5e7f9443f6d3c5265c3eb024b333ac71bfc77650
Terry Tan <59902670+terrytjw@users.noreply.github.com>
2023-03-26 15:23:41 +0800

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import { Theme, ThemeContext } from "../lib/context";
+import { useState, useEffect } from "react";
+import Footer from "../components/Footer";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches) // using the Window.matchMeida() api to check the OS theme
+    ) {
+      setTheme("dark");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={theme}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default MyApp;
